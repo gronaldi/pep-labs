cca5cea4b85d201c797cd4547e6a926391a9be49
gronaldi <gronaldi98@gmail.com>
2022-12-28 08:27:41 +0000

    Saved progress on Instantiation 2022-12-28 08:27:41 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        final object object = new object();
+        return new object();
     }
 }

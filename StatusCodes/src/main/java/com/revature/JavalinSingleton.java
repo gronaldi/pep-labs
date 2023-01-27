d3745aa230996a276d0fde04af5d7f23fbb6711e
gronaldi <gronaldi98@gmail.com>
2023-01-27 07:00:40 +0000

    Saved progress on PathParameters 2023-01-27 07:00:40 +0000

--- StatusCodes/src/main/java/com/revature/JavalinSingleton.java
+++ StatusCodes/src/main/java/com/revature/JavalinSingleton.java
@@ -13,10 +13,12 @@ public class JavalinSingleton {
          * Note: Please refer to the "StatusCodes.MD" file for more assistance if needed.
          */
         app.get("/client-side-err", ctx -> {
-        
+            ctx.status(400);
+            ctx.result("Bad Request: Invalid or malformed request.");
+        });
             //write logic here
 
-        });
+        
 
         return app;
     }

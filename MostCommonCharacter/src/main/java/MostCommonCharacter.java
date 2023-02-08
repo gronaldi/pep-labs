5ba155d4c41dcec3cc94a9758822856df589470c
gronaldi <gronaldi98@gmail.com>
2023-02-08 07:39:34 +0000

    Saved progress on MostCommonCharacter 2023-02-08 07:39:34 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +10,17 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        Map<Character, Integer> map = new HashMap<>();
+    for (char c : str.toCharArray()) {
+        map.put(c, map.getOrDefault(c, 0) + 1);
     }
-}
+    int maxCount = 0;
+    char maxChar = ' ';
+    for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+        if (entry.getValue() > maxCount) {
+            maxCount = entry.getValue();
+            maxChar = entry.getKey();
+        }
+    }
+    return maxChar;
+}}
